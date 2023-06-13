8a053b90f97feb24b283a67e911d7cabcd257896
Daniel Orner <daniel.orner@flipp.com>
2023-06-13 14:29:54 -0400

    tests are invisible!

--- go/test/test.sh
+++ go/test/test.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 
+go install github.com/jstemmer/go-junit-report/v2@latest
+
 TAGS=$1
 TIMEOUT=$2
 
-cmd="go test ./... -coverprofile=coverage.out -json"
+cmd="go test 2>&1 ./... -coverprofile=coverage.out"
 
 if [ -n "$TAGS" ]; then
   cmd="$cmd --tags=${TAGS}"
@@ -13,7 +15,7 @@ if [ -n "$TIMEOUT" ]; then
   cmd="$cmd -timeout ${TIMEOUT}s"
 fi
 
-cmd="${cmd} > test-report.out"
+cmd="${cmd} | go-junit-report -set-exit-code > report.xml"
 
 echo "$cmd"
 eval "$cmd"
