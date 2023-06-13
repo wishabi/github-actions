ddd543d95bc196bd6cb77693414dcc84127f7a4f
Daniel Orner <daniel.orner@flipp.com>
2023-06-13 16:37:41 -0400

    Add tags so we can run integration tests (#108)
    * Add tags so we can run integration tests
    Use junit so we can see test output and save it at the same time
    
    * Fix for sonarqube
    
    * Second fix for sonarqube

---
+++ go/test/test.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+TAGS=$1
+TIMEOUT=$2
+
+cmd="go test 2>&1 ./... -coverprofile=coverage.out"
+
+if [ -n "$TAGS" ]; then
+  cmd="$cmd --tags=${TAGS}"
+fi
+
+if [ -n "$TIMEOUT" ]; then
+  cmd="$cmd -timeout ${TIMEOUT}s"
+fi
+
+cmd="${cmd} | tee test-report.out"
+
+echo "$cmd"
+eval "$cmd"
