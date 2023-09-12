0de4bcedd981328ec846e2b6d1bb07650fc34d65
Daniel Orner <daniel.orner@flipp.com>
2023-09-12 13:29:05 -0400

    PD-376: Use coverpkg to specify all local packages

--- go/test/test.sh
+++ go/test/test.sh
@@ -6,6 +6,23 @@ TIMEOUT=$2
 
 cmd="go test 2>&1 ./... -coverprofile=coverage.out"
 
+# find non-ignored subdirectories
+file_list=()
+
+for dir in */; do
+   if ! git check-ignore -q "$dir"; then
+    file_list+=("./$dir...")
+   fi
+done
+
+# set delimiter to , so we can join the list with commas
+old_ifs=$IFS
+IFS=,
+package_list="${file_list[*]}"
+IFS=$old_ifs # put delimiter back to the original value
+
+cmd="$cmd -coverpkg $package_list"
+
 if [ -n "$TAGS" ]; then
   cmd="$cmd --tags=${TAGS}"
 fi
